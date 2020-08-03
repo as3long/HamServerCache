37572d9f8083b1880438e2e46136f089d7aaac46
as3long <as3long@qq.com>
2020-08-03 16:29:57 +0800

    提交代码

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,8 @@
 import hamserver from 'hamserver';
 import config from './config.mjs';
 
-console.log(hamserver);
 const server = new hamserver.Server(config);
 
 server.start();
+
+console.log('程序已经启动，环境变量', process.env);
