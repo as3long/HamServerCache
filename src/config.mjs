dda43dbc37cc5dd18a9573df0e297965f652e714
as3long <as3long@qq.com>
2020-08-03 16:07:15 +0800

    修改端口号

--- src/config.mjs
+++ src/config.mjs
@@ -3,7 +3,7 @@ import hamserver from 'hamserver';
 
 export default {
     ip: '0.0.0.0',
-    port: 18206,
+    port: 38207,
     mongodb: 'mongodb://localhost/hamservercache',
     model: 'cache',
     controllerPath: path.resolve(hamserver.Utils.getDirName(import.meta.url), './controller'),
