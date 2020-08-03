e40050039387e32d088e103287588a8cd883721e
as3long <as3long@qq.com>
2020-08-03 16:47:30 +0800

    修改配置

--- src/config.mjs
+++ src/config.mjs
@@ -3,7 +3,7 @@ import hamserver from 'hamserver';
 
 export default {
     ip: '0.0.0.0',
-    port: 38207,
+    port: process.env.WALLE_PORT || 38207, //瓦力部署系统上可以添加全局变量
     mongodb: 'mongodb://localhost/hamservercache',
     model: 'cache',
     controllerPath: path.resolve(hamserver.Utils.getDirName(import.meta.url), './controller'),
